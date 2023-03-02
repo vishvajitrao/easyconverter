8e4251634ff55a8624e0a810b4c1a698b34e7a5e
Vishvajit Rao <vishvajitrao@gmail.com>
2023-03-02 20:53:41 +0530

    comment added in admin file

--- converterTool/converter/admin.py
+++ converterTool/converter/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from converter.models import *
 from import_export.admin import ImportExportModelAdmin
 
-
+# this is for admin
 admin.site.register(Ads)
 admin.site.register(Advertisement)
 admin.site.register(Post)
